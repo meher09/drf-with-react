8fe35bbde4298ae1c654cbb49c530f8a21849453
meher09 <mehernigarcu@gmail.com>
2022-11-20 00:31:39 +0600

    first commit

---
+++ api_backend/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+from django.contrib.auth.models import BaseUserManager
+
+
+# Create your models here.
+class UserManager(BaseUserManager):
+
+    def create_user(self, username, email, password=None, **kwargs):
+        """Create and return a `User` with an email, phone number, username and password."""
+        if username is None:
+            raise TypeError('Users must have a username.')
+        if email is None:
+            raise TypeError('Users must have an email.')
+
+        user = self.model(username=username, email=self.normalize_email(email))
+        user.set_password(password)
+        user.save(using=self._db)
+
+        return user
